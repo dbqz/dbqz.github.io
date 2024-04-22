8c8645449fd1cae27b84ad855d226054fe15bb42
db <154511963+dbqz@users.noreply.github.com>
2024-04-22 21:39:41 +0800

    Delete zhq directory

--- zhq/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testPathIgnorePatterns: ['/build/', '/dist/', '/node_modules/'],
-  setupFilesAfterEnv: ['./src/__test__/setup_jest.js'],
-  moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1',
-  },
-};
