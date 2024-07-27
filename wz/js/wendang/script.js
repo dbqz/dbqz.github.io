e760498b9a506d634c0dda7b01f276623608803d
db <154511963+dbqz@users.noreply.github.com>
2024-07-28 07:38:57 +0800

    Update script.js

--- wz/js/wendang/script.js
+++ wz/js/wendang/script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (username === 'wangtao' && password === 'wangtao') {
             window.location.href = "/wendang/web/61082520100415366X/"; // 替换为你的目标网址
         } else {
-            alert('账号或密码错误，请联系管理员重置密码！');
+            alert('账号或密码错误，老弟没实力就别登！');
         }
     });
 });
