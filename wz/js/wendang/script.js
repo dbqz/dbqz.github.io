0a46ef21414fb154d98b29c5553704549157f50e
db <154511963+dbqz@users.noreply.github.com>
2024-07-28 07:45:10 +0800

    Update script.js

--- wz/js/wendang/script.js
+++ wz/js/wendang/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const password = document.getElementById('password').value;
 
         if (username === 'wangtao' && password === 'wang112233@') {
-            window.location.href = "/wendang/web/61082520100415366X/"; // 替换为你的目标网址
+            window.location.href = "/wendang/web/61082520100415366X/web/"; // 替换为你的目标网址
         } else {
             alert('账号或密码错误，老弟没实力就别登！');
         }
