a9a7953c06398c3aece7378b2150622c7c9bc74d
db <154511963+dbqz@users.noreply.github.com>
2024-07-28 07:42:59 +0800

    Update script.js

--- wz/js/wendang/script.js
+++ wz/js/wendang/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
 
-        if (username === 'wangtao' && password === 'wangtao') {
+        if (username === 'wangtao' && password === 'wang112233@') {
             window.location.href = "/wendang/web/61082520100415366X/"; // 替换为你的目标网址
         } else {
             alert('账号或密码错误，老弟没实力就别登！');
