fda53a3a2a54ddb5f3676a1cc8b09dbdfbdb6bf7
王滔 <154511963+dbqz@users.noreply.github.com>
2024-09-24 13:00:59 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-// sw.js
 self.addEventListener('install', function(event) {
-  // 安装阶段可以做一些初始化工作，但这里不指定任何静态资源
   console.log('Service Worker installed');
+  // 调用 skipWaiting 来让 service worker 立即激活
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', function(event) {
-  // 激活阶段可以清理旧的缓存
+  console.log('Service Worker activated');
+  // 清除旧缓存，只保留最新版本
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.filter(function(cacheName) {
+          // 删除除了当前版本的缓存之外的所有缓存
           return cacheName !== 'dynamic-cache';
         }).map(function(cacheName) {
           return caches.delete(cacheName);
@@ -20,23 +22,24 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  // 监听每一个fetch事件，并尝试从缓存中获取资源
+  console.log(`Fetching resource: ${event.request.url}`);
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      // 如果缓存中有资源，则直接返回
+      // 查找缓存中的响应
       if (response) {
         return response;
       }
 
-      // 否则，从网络获取，并将响应存储到缓存中
+      // 如果缓存中没有，从网络获取数据
       return fetch(event.request).then(function(networkResponse) {
         if (!networkResponse || networkResponse.status !== 200) {
           return networkResponse;
         }
 
-        // 克隆响应，以便将克隆后的响应存储到缓存中
-        var responseToCache = networkResponse.clone();
+        // 克隆响应以用于缓存
+        let responseToCache = networkResponse.clone();
         caches.open('dynamic-cache').then(function(cache) {
+          // 将响应添加到缓存中
           cache.put(event.request, responseToCache);
         });
 
